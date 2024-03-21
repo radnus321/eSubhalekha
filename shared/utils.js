4901399f98c8ba805fa11db19eba6783d493dd85
radnus321 <f20220165@pilani.bits-pilani.ac.in>
2024-03-21 22:18:50 +0530

    sunflower with shadows

--- shared/utils.js
+++ shared/utils.js
@@ -1,24 +1,7 @@
-/*
- * Copyright 2017 Google Inc. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the 'License');
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an 'AS IS' BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 window.gltfLoader = new THREE.GLTFLoader();
-/**
- * The Reticle class creates an object that repeatedly calls
- * `xrSession.requestHitTest()` to render a ring along a found
- * horizontal surface.
- */
+
 class Reticle extends THREE.Object3D {
   constructor() {
     super();
@@ -45,7 +28,6 @@ window.DemoUtils = {
    * and a mesh that will receive shadows.
    *
    * @return {THREE.Scene}
-   */
   createLitScene() {
     const scene = new THREE.Scene();
 
@@ -121,10 +103,6 @@ window.DemoUtils = {
   },
 };
 
-/**
- * Toggle on a class on the page to disable the "Enter AR"
- * button and display the unsupported browser message.
- */
 function onNoXRDevice() {
   document.body.classList.add('unsupported');
 }
